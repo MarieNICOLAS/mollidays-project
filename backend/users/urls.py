0e739a6b30228017a2bbd781de1eb05604089215
MarieNICOLAS <marie.nicolas29@hotmail.com>
2025-03-21 00:12:42 +0100

    feat(auth): add JWT-protected route

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserListView
+from .views import UserListView, protected_view
 
 urlpatterns = [
     path('users/', UserListView.as_view(), name='user-list'),
+    path('protected/', protected_view, name='protected'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,9 +1,18 @@
 from rest_framework import generics
 from django.contrib.auth import get_user_model
 from .serializer import UserSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 User = get_user_model()
 
 class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def protected_view(request):
+    return Response({"message": f"Hello {request.user.email}, tu es bien authentifié !"})
