916b74f96ceb88806b0ec9927711201cd35c3510
Myo <marie.nicolas29@hotmail.com>
2025-05-15 18:19:02 +0200

    feat(user): updated User model and serializer with full profile fields

--- backend/users/serializer.py
+++ backend/users/serializer.py
@@ -1,4 +1,3 @@
-from dataclasses import field
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
@@ -7,4 +6,8 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_active']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'account_status', 'role']
+        extra_kwargs = {
+            'account_status':{'read_only': True},
+            'role': {'read_only': True}
+        }
