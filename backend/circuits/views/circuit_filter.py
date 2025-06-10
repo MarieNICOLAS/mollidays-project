cba1361a0ff9d3c8a5226a7c037ef5bcb4093fca
Myo <marie.nicolas29@hotmail.com>
2025-06-11 01:48:46 +0200

    feat(circuits): add filtering endpoint with support for price, duration, category, tags and destination

--- backend/circuits/views/circuit_filter.py
+++ backend/circuits/views/circuit_filter.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from circuits.models.circuit import Circuit
 from circuits.serializers.circuit_serializer import CircuitSerializer
+from datetime import date
 
 class CircuitFilteredAPIView(generics.ListAPIView):
     serializer_class = CircuitSerializer
@@ -16,19 +17,34 @@ class CircuitFilteredAPIView(generics.ListAPIView):
             qs = qs.filter(category__name__icontains=category)
 
         if min_price := params.get("min_price"):
-            qs = qs.filter(price__gte=min_price)
+            try:
+                qs = qs.filter(price__gte=float(min_price))
+            except ValueError:
+                pass
 
         if max_price := params.get("max_price"):
-            qs = qs.filter(price__lte=max_price)
+            try:
+                qs = qs.filter(price__lte=float(max_price))
+            except ValueError:
+                pass
 
         if min_duration := params.get("min_duration"):
-            qs = qs.filter(duration__gte=min_duration)
+            try:
+                qs = qs.filter(duration__gte=int(min_duration))
+            except ValueError:
+                pass
 
         if max_duration := params.get("max_duration"):
-            qs = qs.filter(duration__lte=max_duration)
+            try:
+                qs = qs.filter(duration__lte=int(max_duration))
+            except ValueError:
+                pass
 
         if start_date := params.get("start_date"):
-            qs = qs.filter(start_date__gte=start_date)
+            try:
+                qs = qs.filter(start_date__gte=date.fromisoformat(start_date))
+            except ValueError:
+                pass
 
         if tags := params.getlist("tags"):
             qs = qs.filter(tags__name__in=tags)
